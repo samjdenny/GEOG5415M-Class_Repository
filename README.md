8528ff96dfeef3cdfe658efaa2a21fcf2b1a08c3
Nick Malleson <6339379+nickmalleson@users.noreply.github.com>
2025-11-06 18:27:00 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GEOG5415M Class Repository
+
+Basic repository that contains a file with some points in and a notebook to read and display the points.
---
+++ points.py
@@ -0,0 +1,23 @@
+# points.py
+# A simple py file that contains a list of points and a function that reads
+# the list and returns the points
+
+# ****************** EDIT BELOW ******************
+# Add your point as a new item in the list. 
+# Create a new line for your point, and don't forget the square brackets and the comma
+# Poinst should be in the following format:
+# [ latitude, longitude, "description" ]
+
+points_list = [
+    [  53.808478, -1.5527924, "University of Leeds" ],
+    [  53.840032, -1.6432286, "Nick's neighbourhood" ],
+]
+
+
+
+
+# ****************** EDIT ABOVE ******************
+
+def get_points():
+    """Return the list of points"""
+    return points_list
